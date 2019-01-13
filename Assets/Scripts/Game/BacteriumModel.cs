c55535863124c9a808ff836d71d40c2dc05e4e82
alexsey chuvilkin <leha.chuvilkin.90@mail.ru>
2019-01-14 01:23:16 +0300

    Virus Group Timer

---
+++ Assets/Scripts/Game/BacteriumModel.cs
@@ -0,0 +1,7 @@
+﻿using GameCore.Model;
+
+public class BacteriumModel : BacteriumBase
+{
+    public BacteriumModel(int roadsCount, BacteriumData bacteriumData) : base(roadsCount, bacteriumData) { }
+}
+
